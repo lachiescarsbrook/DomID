3329c2d15458509a4b340462e9dc656d8757c113
lachiescarsbrook <58501490+lachiescarsbrook@users.noreply.github.com>
2025-12-09 11:02:31 +0000

    Create adapter_removal_PE.sh

---
+++ workflow/scripts/adapter_removal_PE.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+IN=$1
+LIBRARY=$2
+#Searches input directory for fastq files with the given sample ID
+FILES=$(find "$IN" -type f \( -name "*.fastq*" -o -name "*.fq*" \) | grep -E "${LIBRARY}([_.-]|$)" | sort)
+#Assigns paired-end reads to separate variables 
+READ1=$(echo "$FILES" | sed -n '1p')
+READ2=$(echo "$FILES" | sed -n '2p')
+#Removes adapters, low quality, and missing bases from paired-end reads, and collapses overlapping reads
+AdapterRemoval --file1 $READ1 --file2 $READ2 --collapse --trimns --trimqualities --gzip --basename results/adrm/${LIBRARY}_PE
